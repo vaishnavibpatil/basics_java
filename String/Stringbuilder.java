40663f285eda63cf5e5ba632a2f5925a6053250e
vaishnavibpatil <116648404+vaishnavibpatil@users.noreply.github.com>
2024-02-28 22:57:04 +0530

    Add files via upload

---
+++ String/Stringbuilder.java
@@ -0,0 +1,13 @@
+public class Stringbuilder
+{
+    public static void main(String args[])
+    {
+        StringBuilder sb= new StringBuilder("");
+        for(char ch='a';ch<='z';ch++)
+        {
+            sb.append(ch);
+        }
+        System.out.print(sb.length());
+    }
+    
+}
