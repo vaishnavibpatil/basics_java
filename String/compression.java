76fbc94e176a06a9f256d6f8d6fcd2977750c123
vaishnavibpatil <116648404+vaishnavibpatil@users.noreply.github.com>
2024-02-29 23:40:06 +0530

    Add files via upload

---
+++ String/compression.java
@@ -0,0 +1,30 @@
+public class compression 
+{
+    public static String compression(String str)
+    {
+        StringBuilder sb = new StringBuilder("");
+        for(int i=0;i<str.length();i++)
+        {
+            Integer count=1;
+            while(i < str.length()-1 && str.charAt(i) == str.charAt(i+1))
+            {
+                count++;
+                i++;
+            }
+            sb.append(str.charAt(i));
+            if(count > 1)
+            {
+                sb.append(count);
+            }
+      
+        }
+        return sb.toString();
+    }
+    public static void main(String args[])
+    {
+        String str = new String("aaabbccc");
+        System.out.print(compression(str));
+        
+    }
+    
+}
