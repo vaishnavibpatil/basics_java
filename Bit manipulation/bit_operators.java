5d5d5d62101e2c8158ec3ae8aba55a290db1d291
vaishnavibpatil <116648404+vaishnavibpatil@users.noreply.github.com>
2024-03-01 20:57:53 +0530

    Add files via upload

---
+++ Bit manipulation/bit_operators.java
@@ -0,0 +1,25 @@
+public class bit_operators 
+{
+    public static void main(String[] args)
+    {
+
+     //bitwise AND operator
+       System.out.println("5 & 6 ="+(5 & 6));
+
+       //bitwise OR operator
+       System.out.println("5 | 6 = "+( 5 | 6));
+
+       //bitwise XOR operator
+       System.out.println("5 ^ 6 = "+(5 ^ 6));
+
+       //bitwise one's complement operator
+       System.out.println("~5 = "+(~5));
+
+       //bitwise left shift operator
+       System.out.println("5 << 2 = "+ (5 << 2));
+
+       //bitwise right shift operator
+       System.out.println("6 >> 1 = " +(6 >> 1));
+    }
+    
+}
