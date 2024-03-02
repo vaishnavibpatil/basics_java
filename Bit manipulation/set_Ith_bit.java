5e24ab13e7ed064495e9c72c8f0306fdfc4d65c9
vaishnavibpatil <116648404+vaishnavibpatil@users.noreply.github.com>
2024-03-02 21:59:49 +0530

    Add files via upload

---
+++ Bit manipulation/set_Ith_bit.java
@@ -0,0 +1,14 @@
+public class set_Ith_bit
+{
+    public static int setIthBit(int n,int i)
+    {
+        int bitMask =  1 << i;
+        return ( n | bitMask ) ;
+
+    }
+    public static void main(String[] args)
+    {
+        System.out.println(setIthBit(10,2));
+    }
+    
+}
