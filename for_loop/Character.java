905850eb031d49d68642adddac4ed6c5019505c2
vaishnavibpatil <116648404+vaishnavibpatil@users.noreply.github.com>
2024-03-27 23:31:43 +0530

    Add files via upload

---
+++ for_loop/Character.java
@@ -0,0 +1,14 @@
+public class Character 
+{
+    public static void main(String args[])
+    {
+        char ch = 'A';
+        while(ch <= 'Z')
+        {
+            System.out.print(ch + " ");
+            ch++;
+        }
+       
+    }
+    
+}
