20cd9d12f3d295f19dfcf6c75414663f9c8b6d1f
vaishnavibpatil <116648404+vaishnavibpatil@users.noreply.github.com>
2024-04-18 22:46:26 +0530

    Add files via upload

---
+++ Divide and conquer/Quicksort.java
@@ -0,0 +1,48 @@
+public class Quicksort 
+{
+    public static void print(int arr[])
+    {
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i] + " ");
+        }
+    }
+    public static void quick(int arr[],int si,int ei)
+    {
+        if(si>=ei)
+        {
+            return;
+        }
+        int pIdx = partition(arr,si,ei);
+        quick(arr,si,pIdx-1);
+        quick(arr,pIdx+1,ei);
+    }
+   public static int partition(int[] arr,int si,int ei)
+   {
+        int pivot=arr[ei];
+        int i = si-1;
+        for(int j=si;j<ei;j++)
+        {
+            if(arr[j]<=pivot)
+            {
+                i++;
+                int temp=arr[j];
+                arr[j]=arr[i];
+                arr[i]=temp;
+            }
+        }
+        i++;
+        int temp=pivot;
+        arr[ei]=arr[i];
+        arr[i]=temp;
+        return i;    
+   }
+
+    public static void main(String args[])
+    {
+        int arr[]={6,3,9,8,2,5};
+        quick(arr,0,arr.length-1);
+        print(arr);
+    }
+    
+}
